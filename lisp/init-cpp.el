29c3b08d8218dc6976d2989f251a25f0fb99e2ac
lolosssss <814364672@qq.com>
2016-01-21 12:23:13 +0800

    .emacs.d on mac os x

---
+++ lisp/init-cpp.el
@@ -0,0 +1,23 @@
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(defun my:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/7.0.2/include")
+  )
+
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+
+;; C-c ; to multi edit
+(define-key global-map (kbd "C-c ;") 'iedit-mode)
+
+;; google-c-style
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+(provide 'init-cpp)
+;;; init-cpp ends here

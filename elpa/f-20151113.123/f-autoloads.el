0e858272db08491c9f3066f4f4f101cfe6479b76
lolosssss <814364672@qq.com>
2016-01-21 12:31:55 +0800

    add elpa

---
+++ elpa/f-20151113.123/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("f.el") (22174 20122 90740 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
